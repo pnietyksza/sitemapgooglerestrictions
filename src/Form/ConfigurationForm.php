ea4e839521835715c9319ee7d3cce10ae184102a
Patryk <patryk.nietyksza@gmail.com>
2023-11-05 23:50:57 +0100

    create ConfigurationForm

---
+++ src/Form/ConfigurationForm.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace SitemapGoogleRestrictions\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ConfigurationForm extends AbstractType{
+    public function buildform(FormBuilderInterface $builder, array $options){
+        
+    }
+}
